efcca2eba990bbf91c356890ff743d68f66830ef
Tachidito <85906328+AxMar1221@users.noreply.github.com>
2024-09-09 12:03:32 -0600

    new html

---
+++ example3.js
@@ -0,0 +1,12 @@
+// Simulación de registros
+let registro1 = 8;
+let registro2 = 12;
+
+// Intercambio de valores entre los registros
+let temp = registro1;
+registro1 = registro2;
+registro2 = temp;
+
+console.log("Después del intercambio, registro1:", registro1); // Salida: 12
+console.log("Después del intercambio, registro2:", registro2); // Salida: 8
+
