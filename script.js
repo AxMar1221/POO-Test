a249fdce1be749aaa30c8ee4c10058b175e69cad
Tachidito <85906328+AxMar1221@users.noreply.github.com>
2024-09-09 12:50:33 -0600

    cosole.log remove

--- script.js
+++ script.js
@@ -18,9 +18,7 @@ function ejecutar() {
     try {
         // Evaluar el código y capturar el resultado
         const resultado = eval(codigo);
-        
-        // Mostrar el resultado en la consola del navegador
-        console.log('Resultado:', resultado);
+
     } catch (error) {
         // Mostrar errores en la consola del navegador
         console.error('Error:', error.message);
